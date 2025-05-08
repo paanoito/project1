10fd6216cab737a9d2f47f092735e725807c22b6
paanoito <thizme0@gmail.com>
2025-05-08 23:52:42 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using Final_Project;
+using Final_Project.Presenters;
+using Final_Project.Views;
+
+namespace Final_Project
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form2 form = new Form2();
+            MenuPresenter presenter = new MenuPresenter(form);
+            Application.Run(form);
+        }
+    }
+}
